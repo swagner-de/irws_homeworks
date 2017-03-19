e25bc0a95b3fd26e4655c52a59ea96ae8d8a3066
swagner-de <sebastian-wagner@outlook.com>
2017-03-19 15:07:39 +0100

    modified test script

--- levenshtein-c/test.sh
+++ levenshtein-c/test.sh
@@ -1,5 +1,5 @@
 ./levenshtein_c unweighted amarillone smatillimr
 ./levenshtein_c unweighted maarillone amatilulimr
-./levenshtein_c weighted amarillone smtillimr ../test_weights.json
-./levenshtein_c weighted maarillone amatilulimr ../test_weights.json
-./levenshtein_c weighted saarillone asstilkimr ../test_weights.json
+./levenshtein_c weighted amarillone smtillimr $1
+./levenshtein_c weighted maarillone amatilulimr $1
+./levenshtein_c weighted saarillone asstilkimr $1
